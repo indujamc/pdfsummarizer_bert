71fb31aa3957924f30f314e9d004970c704cca04
indujamc <167214685+indujamc@users.noreply.github.com>
2024-07-12 15:20:01 +0530

    Update sum4.py

--- sum4.py
+++ sum4.py
@@ -8,7 +8,8 @@ st.write("""
 """)
 text_input = st.text_area("Text to summarize")
 
-summary = model(text_input, num_sentences=4)
+# Use the model to summarize the input text
+summary = model(text_input)
 
 st.write("## Summarized Text")
 st.write(f"<div style='font-family: Arial;'> {''.join(summary)} </div>", unsafe_allow_html=True)
