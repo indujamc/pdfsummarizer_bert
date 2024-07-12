09f0ec356959950e16836eba9a81f28bda32bd99
indujamc <167214685+indujamc@users.noreply.github.com>
2024-07-12 15:18:52 +0530

    Update sum4.py

--- sum4.py
+++ sum4.py
@@ -1,29 +1,14 @@
 import streamlit as st
 from summarizer import Summarizer
 
-# Initialize the Summarizer model
 model = Summarizer()
 
-# Streamlit app header
 st.write("""
 # Summarize Your Text
 """)
-
-# Text input area for user input
 text_input = st.text_area("Text to summarize")
 
-# Check if there's input to summarize
-if st.button("Summarize"):
-    # Ensure text_input is not empty
-    if text_input.strip():
-        try:
-            # Generate the summary using the Summarizer model
-            summary = model.predict(text_input)
-            
-            # Display the summarized text
-            st.write("## Summarized Text")
-            st.write(summary)
-        except Exception as e:
-            st.error(f"An error occurred: {e}")
-    else:
-        st.warning("Please enter some text to summarize.")
+summary = model(text_input, num_sentences=4)
+
+st.write("## Summarized Text")
+st.write(f"<div style='font-family: Arial;'> {''.join(summary)} </div>", unsafe_allow_html=True)
