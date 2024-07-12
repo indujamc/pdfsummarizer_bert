b91a2e5f8628d55d56d87d81e34dcb99ef1bf91b
indujamc <167214685+indujamc@users.noreply.github.com>
2024-07-12 15:20:46 +0530

    Update sum4.py

--- sum4.py
+++ sum4.py
@@ -1,15 +1,24 @@
 import streamlit as st
 from summarizer import Summarizer
 
+# Initialize the Summarizer model
 model = Summarizer()
 
+# Streamlit UI
 st.write("""
 # Summarize Your Text
 """)
 text_input = st.text_area("Text to summarize")
 
-# Use the model to summarize the input text
-summary = model(text_input)
-
-st.write("## Summarized Text")
-st.write(f"<div style='font-family: Arial;'> {''.join(summary)} </div>", unsafe_allow_html=True)
+# Check if there is input to summarize
+if st.button("Summarize"):
+    # Ensure text_input is not empty
+    if text_input:
+        # Generate summary using the model
+        summary = model(text_input)
+        
+        # Display the summarized text
+        st.write("## Summarized Text")
+        st.write(f"<div style='font-family: Arial;'> {''.join(summary)} </div>", unsafe_allow_html=True)
+    else:
+        st.write("Please enter some text to summarize.")
